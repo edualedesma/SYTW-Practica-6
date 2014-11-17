4b4d13f0f0264f8cea97d4eee466402a4c5e92f8
Sergio <alu0100696615@ull.edu.es>
2014-11-17 14:55:27 +0000

    .

--- chat.rb
+++ chat.rb
@@ -5,7 +5,7 @@ require 'sinatra/reloader' if development?
 
 chat = ['welcome..']
 
-get('/') { erb :index }
+get('/chat') { erb :index }
 
 get '/send' do
   return [404, {}, "Not an ajax request"] unless request.xhr?
