ad149a5eeead625cc3216ecfe292b2d33aef6dad
Eduardo <eaculed@gmail.com>
2014-11-19 15:34:46 +0000

    Mejorando registro e implementando logout con sesiones

--- chat.rb
+++ chat.rb
@@ -10,23 +10,43 @@ chat = ['welcome..']
 
 usuarios = []
 
+enable :sessions
+set :session_secret, '*&(^#234a)'
+
 get '/' do
-  erb :register
+  if (!session[:user]) #Comprobamos si el usuario ya se registro o no.
+    erb :signup
+  else
+    erb :index
+  end
+  
 end
 
-post '/chat' do
-
-  # Capturar el nombre del usuario
-  puts "Dentro de los parametros: #{params}"
-  usuarios.push(URI::parse(params[:usuario]).to_s)
-  puts "Estos son los usuarios registrados: #{usuarios}"
-  puts "Los usuarios ahora mismo son"
-  puts usuarios
+get '/chat' do
   erb :index
 end
 
-get '/login' do
-  erb :login
+post '/chat' do
+  # Capturar el nombre del usuario
+  #puts "Dentro de los parametros: #{params}"
+  user = URI::parse(params[:usuario]).to_s
+  if (usuarios.include?(user))
+    redirect '/'
+  else
+    usuarios.push(user)
+    session[:user] = user
+    puts "Este es el usuario de la sesion: #{session[:user]} y de la variable: #{user}"
+  #puts "Estos son los usuarios registrados: #{usuarios}"
+  puts "Los usuarios ahora mismo son"
+  puts usuarios
+    erb :index
+  end
+end
+
+get '/logout' do
+  usuarios.delete(session[:user])
+  session.clear
+  redirect '/'
 end
 
 get '/send' do
@@ -43,6 +63,7 @@ get '/pedirusuarios' do
    enviar = enviar + "</ul>"
    enviar.to_s
 end
+
 get '/update' do
   return [404, {}, "Not an ajax request"] unless request.xhr?
   @updates = chat[params['last'].to_i..-1] || []
