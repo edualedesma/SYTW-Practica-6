3287acdec87faf6fc38d75f36eaea77ddee058b5
Sergio <alu0100696615@ull.edu.es>
2014-11-21 00:38:15 +0000

    Modificando pruebas spec

--- spec/spec.rb
+++ spec/spec.rb
@@ -13,13 +13,15 @@ def app
 end
 describe "Rspec" do
 
-    it "/update" do
-		get '/update'
-		expect(last_response.body).to eq("Not an ajax request")
-	end
 
 	it "/send" do
 		get '/send'
+		expect(last_response.body).to eq("Not an ajax request")
+	end
+
+
+    it "/update" do
+		get '/update'
 		expect(last_response.body).to eq("Not an ajax request")
 	end
 
@@ -28,4 +30,33 @@ describe "Rspec" do
 		expect(last_response).to be_ok
 	end
 
- end
+	it "/" do
+		get '/'
+		last_response.body['Login']
+	end	
+
+    it "/send" do
+       get '/send', {}, 'rack.session' => { :name => 'Testing' }
+       expect(last_response.body).to eq("Not an ajax request")
+    end
+    
+    it "Update" do
+       get'/update'
+       expect(last_response.body).to eq("Not an ajax request")
+    end
+    
+	it "/PedirUsuarios" do
+	    get '/pedirusuarios'
+	    expect(last_response).to be_ok
+	end
+
+    it "HTTP_X_REQUESTED_WITH Update" do
+		get '/update',{}, {"HTTP_X_REQUESTED_WITH" => "XMLHttpRequest"}
+		expect(last_response).to be_ok
+	end
+	
+
+	
+
+end
+
